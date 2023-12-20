c42cf4cc40c4917ed65db67efbd170fcf8d4b315
Laba Moirangthem <labamthem4@gmail.com>
2023-12-20 15:01:41 +0530

    Hello

---
+++ calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("Enter two number");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	d=a-b;
+	printf("addition is %d and differnce is %d ",c,d);
+	return 0;
+}
